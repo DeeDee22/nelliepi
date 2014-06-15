655b3199d340a317868eeaf3e18c494d066881c0
geraldine <geraldine@fuchur>
2014-06-15 18:24:15 +0200

    added ip address to start screen

--- src/ch/fluxkompensator/nelliepi/ui/screen/StartScreen.py
+++ src/ch/fluxkompensator/nelliepi/ui/screen/StartScreen.py
@@ -3,9 +3,13 @@ Created on Oct 4, 2013
 
 @author: geraldine
 '''
-from ch.fluxkompensator.nelliepi.ui.screen.ScreenWithFooter import ScreenWithFooter
-from ch.fluxkompensator.nelliepi.ui.widgets.TextButton import TextButton
+from ch.fluxkompensator.nelliepi import IPAddressFinder
 from ch.fluxkompensator.nelliepi.functions import ChangeScreenFunction
+from ch.fluxkompensator.nelliepi.ui.screen.ScreenWithFooter import \
+    ScreenWithFooter
+from ch.fluxkompensator.nelliepi.ui.widgets.TextButton import TextButton
+from ch.fluxkompensator.nelliepi.Constants import RED
+
 
 class StartScreen(ScreenWithFooter):
     '''
@@ -22,6 +26,11 @@ class StartScreen(ScreenWithFooter):
         musicButton = TextButton(self.getMaxWidth() / 2, self.getMaxHeight() / 4, "Music", pMethod=ChangeScreenFunction.function, pParams=["musicScreen",])
         self.addButton(musicButton)
         
-        diagnosticsButton = TextButton(self.getMaxWidth() / 2, self.getMaxHeight() / 4 * 3, "Nellies Data")
+        diagnosticsButton = TextButton(self.getMaxWidth() / 2, self.getMaxHeight() / 4 * 2, "Nellies Data")
         self.addButton(diagnosticsButton)
+        
+            
+        ipAddress=IPAddressFinder.get_ip_address("wlan0")
+        ipButton = TextButton(self.getMaxWidth() / 2, self.getMaxHeight() / 4 * 3, "Hi, you can reach me at " + ipAddress, pColor=RED, pFontSize=25)
+        self.addButton(ipButton)
         
